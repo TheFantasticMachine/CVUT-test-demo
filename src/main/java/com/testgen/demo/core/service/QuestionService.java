425737eefeebea50197ef3b94a30917a92dbcee3
FantasticMachine <kotlik.vojtech@juniorlondynska.cz>
2026-07-09 13:40:09 +0200

    idk

--- src/main/java/com/testgen/demo/core/service/QuestionService.java
+++
@@ -1,8 +0,0 @@
-package com.testgen.demo.core.service;
-
-import com.testgen.demo.core.dto.QuestionDto;
-
-public interface QuestionService {
-
-    QuestionDto createQuestion(QuestionDto questionDto);
-}
